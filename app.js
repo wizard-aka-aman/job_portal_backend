7eb30015d1b11482fa089709692b34f62f345519
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-12 22:33:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({ path: "./config/config.env" }); 
 
 app.use(cors({
-    origin : [process.env.FRONTEND_URL],
+    origin : [process.env.FRONTEND_URL , "https://job-portal-backend-uz7u.onrender.com"],
     methods: ['POST' , 'GET' , 'DELETE' , 'PUT'],
     credentials:true
 }))
